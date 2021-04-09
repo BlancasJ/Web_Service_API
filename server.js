df93b38791945cf3a9b1e0bb7bb09f89389f85c0
BlancasJ <Jorge.Blancas@theksquaregroup.com>
2021-04-08 19:30:38 -0500

    try one to deploy

--- server.js
+++ server.js
@@ -6,12 +6,24 @@ const app = express();
 // parse requests of content-type: application/json
 app.use(bodyParser.json());
 
+// cors
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  if ('OPTIONS' == req.method) {
+     res.sendStatus(200);
+   }
+   else {
+     next();
+   }});
+
 // parse request of content-type: application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // simple route
 app.get("/api", (req, res) => {
-  res.json({ message: "Welcome to blancasj app" });
+  res.json({ message: "Welcome to Notes app" });
 });
 
 // routes
